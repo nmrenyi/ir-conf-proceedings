9f8bdc1b2f337e90fbbe887ec5597ec3fa4f1711
nmrenyi <renyi1006@gmail.com>
2022-12-02 23:00:18 +0800

    refactor: refactor script structure

--- src/parse-sigir2022.py
+++ src/parse-sigir2022.py
@@ -12,39 +12,48 @@ def append_paper(paper_list, paper, session_name):
     return paper_list
 
 
-with open('raw-page/sigir2022.html', 'r', encoding='utf-8') as f:
-    soup = BeautifulSoup(f, 'html.parser')
+def main():
+    with open('raw-page/sigir2022.html', 'r', encoding='utf-8') as f:
+        soup = BeautifulSoup(f, 'html.parser')
 
-all_papers = soup.find(id='DLcontent')
+    all_papers = soup.find(id='DLcontent')
 
-info_list = []
-info = {}
-session_name = ''
-
-for i in trange(len(all_papers)):
-    line = all_papers.contents[i]
-    if line.name == 'h2':
-        if info:
-            info_list = append_paper(info_list, info, session_name)
-            info = {}
-            session_name = ''
-        session_name = line.text
-    elif line.name == 'h3':
-        if info:
-            info_list = append_paper(info_list, info, session_name)
-            info = {}
-        info['title'] = line.text
-        info['url'] = line.find('a').get('href')
-    elif line.name == 'ul':
-        info['authors'] = line.find('li').text
-    elif line.name == 'div':
-        info['abstract'] = '\n\n'.join([r.text for r in line.find_all('p')])
-if info:
-    info_list.append(info)
+    info_list = []
     info = {}
-df = pd.DataFrame(info_list)
-print(df.info())
-output_path = 'sigir2022.tsv'
-df[['title', 'authors', 'session', 'url', 'abstract']].to_csv(
-    output_path, sep='\t', index=False)
-print('output saved to {}'.format(output_path))
+    session_name = ''
+
+    for i in trange(len(all_papers)):
+        line = all_papers.contents[i]
+        if line.name == 'h2':
+            if info:
+                info_list = append_paper(info_list, info, session_name)
+                info = {}
+                session_name = ''
+            session_name = line.text
+        elif line.name == 'h3':
+            if info:
+                info_list = append_paper(info_list, info, session_name)
+                info = {}
+            info['title'] = line.text
+            info['url'] = line.find('a').get('href')
+        elif line.name == 'ul':
+            info['authors'] = line.find('li').text
+        elif line.name == 'div':
+            info['abstract'] = '\n\n'.join(
+                [r.text for r in line.find_all('p')])
+
+    if info:
+        info_list.append(info)
+        info = {}
+
+    df = pd.DataFrame(info_list)
+    print(df.info())
+
+    output_path = 'sigir2022.tsv'
+    df[['title', 'authors', 'session', 'url', 'abstract']].to_csv(
+        output_path, sep='\t', index=False)
+    print('output saved to {}'.format(output_path))
+
+
+if __name__ == '__main__':
+    main()
