02f866db35eaa0fb44f0b7e85c15964a5dee446d
nmrenyi <renyi1006@gmail.com>
2022-12-02 23:39:44 +0800

    fix: adjust prompt message

--- src/parse-sigir2022.py
+++ src/parse-sigir2022.py
@@ -16,10 +16,9 @@ def append_paper(paper_list, paper, session_name):
 
 def main():
     url = 'https://sigir.org/sigir2022/program/proceedings/'
-    print('Requesting data... (from {})'.format(url))
 
+    print('Requesting data... (from {})'.format(url))
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    print('Reading data...')
     html_page = urlopen(req).read()
 
     print('Parsing data...')
