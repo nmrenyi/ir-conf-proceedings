2feeb41c52d6e3cc15e5388ecfad2300862b197d
nmrenyi <renyi1006@gmail.com>
2022-12-02 22:54:42 +0800

    feat: fix session missing

--- src/parse-sigir2022.py
+++ src/parse-sigir2022.py
@@ -4,6 +4,12 @@ script of parsing the paper list of SIGIR 2022 from https://sigir.org/sigir2022/
 import pandas as pd
 from bs4 import BeautifulSoup
 
+
+def append_paper(paper_list, paper):
+    paper_list.append(paper)
+    return paper_list
+
+
 with open('raw-page/sigir2022.html', 'r', encoding='utf-8') as f:
     soup = BeautifulSoup(f, 'html.parser')
 
@@ -11,14 +17,23 @@ all_papers = soup.find(id='DLcontent')
 
 info_list = []
 info = {}
+session_name = ''
+
 for i in range(len(all_papers)):
     line = all_papers.contents[i]
-    session_name = ''
     if line.name == 'h2':
-        info['session'] = line.text
+        if info:
+            info['session'] = session_name
+            info_list = append_paper(info_list, info)
+            info = {}
+            session_name = ''
+        session_name = line.text
+        print(session_name)
     elif line.name == 'h3':
         if info:
-            info_list.append(info)
+            info['session'] = session_name
+            print(info['session'], info['title'], info['authors'])
+            info_list = append_paper(info_list, info)
             info = {}
         info['title'] = line.text
         info['url'] = line.find('a').get('href')
