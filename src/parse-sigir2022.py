bc31700cc58f78729e12d119e9566db91c0902b9
nmrenyi <renyi1006@gmail.com>
2022-12-02 21:20:47 +0800

    feat: get all papers

--- src/parse-sigir2022.py
+++ src/parse-sigir2022.py
@@ -1,4 +1,10 @@
 """
 script of parsing the paper list of SIGIR 2022 from https://sigir.org/sigir2022/program/proceedings/
 """
+from bs4 import BeautifulSoup
+
+with open('raw-page/sigir2022.html', 'r', encoding='utf-8') as f:
+    soup = BeautifulSoup(f, 'html.parser')
+
+all_papers = soup.find(id='DLcontent')
 
