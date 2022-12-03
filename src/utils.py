dd70a3773079ab3111b0420f23e506e85a5c5b29
nmrenyi <renyi1006@gmail.com>
2022-12-03 20:18:53 +0800

    feat: add parse_html module

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,18 @@
+from urllib.request import Request, urlopen
+
+from bs4 import BeautifulSoup
+
+
+def parse_html(url, target_id):
+    print('Requesting data... (from {})'.format(url))
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = urlopen(req).read()
+
+    print('Parsing data...')
+    soup = BeautifulSoup(html_page, 'html.parser')
+    return soup.find(id=target_id)
+
+
 def save_file(df, output_path, file_type):
     if file_type == 'tsv':
         df.to_csv(output_path, sep='\t', index=False)
