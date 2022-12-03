6232809f468b2978ece90d3b425a7d704bb3cff6
nmrenyi <renyi1006@gmail.com>
2022-12-03 11:28:54 +0800

    feat: parse papers

--- src/parse-thewebconf2022.py
+++ src/parse-thewebconf2022.py
@@ -8,7 +8,9 @@ def main():
     with open('www2022.html', 'r') as f:
         html_page = f.read()
     soup = BeautifulSoup(html_page, 'html.parser')
-    print(soup.prettify())
+
+    papers = soup.find(id='DLcontent')
+    print(papers)
 
 
 if __name__ == '__main__':
