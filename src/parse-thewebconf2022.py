9d4e9d85adfe5a02356376d73b4fc1448501d105
nmrenyi <renyi1006@gmail.com>
2022-12-03 11:25:58 +0800

    feat: add main function

---
+++ src/parse-thewebconf2022.py
@@ -0,0 +1,11 @@
+"""
+script of parsing the paper list of theWebConf 2022 from https://www2022.thewebconf.org/main-proceedings/
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
