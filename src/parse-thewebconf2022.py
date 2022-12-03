5709f2c4655e66349be6ce7a9927a98326d1316b
nmrenyi <renyi1006@gmail.com>
2022-12-03 11:36:22 +0800

    refactor: finish script structure

--- src/parse-thewebconf2022.py
+++ src/parse-thewebconf2022.py
@@ -1,7 +1,15 @@
 """
 script of parsing the paper list of theWebConf 2022 from https://www2022.thewebconf.org/main-proceedings/
 """
+import argparse
+
+import pandas as pd
 from bs4 import BeautifulSoup
+
+
+def parse_proceeding(proceeding):
+    for track in proceeding.find_all('details'):
+        pass
 
 
 def main():
@@ -10,8 +18,29 @@ def main():
     soup = BeautifulSoup(html_page, 'html.parser')
 
     proceeding = soup.find(id='DLcontent')
-    for track in proceeding.find_all('details'):
-        pass
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', type=str, default='tsv',
+                        help='output file type (default: tsv, options: tsv, md)')
+    args = parser.parse_args()
+
+    info_list = parse_proceeding(proceeding)
+    df = pd.DataFrame(info_list)[
+        ['title', 'authors', 'session', 'url', 'abstract']]
+    print(df.info())
+
+    file_type = args.type
+    output_path = '../data/{0}/thewebconf2022.{0}'.format(file_type)
+
+    if file_type == 'tsv':
+        df.to_csv(output_path, sep='\t', index=False)
+    elif file_type == 'md':
+        with open(output_path, 'w') as f:
+            f.write(df[['title', 'authors', 'session', 'abstract', 'url']].to_markdown(
+                index=False).replace('   ', ''))  # remove redundant whitespace to shrink the file size
+    else:
+        raise ValueError('Unsupported file type: {}'.format(file_type))
+    print('output saved to {}'.format(output_path))
 
 
 if __name__ == '__main__':
