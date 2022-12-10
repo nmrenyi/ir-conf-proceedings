906bce16c70c9b52e76648c6ab0a72005b67747e
nmrenyi <renyi1006@gmail.com>
2022-12-10 10:37:17 +0800

    feat: finish basic file saving

--- src/parse-dblp.py
+++ src/parse-dblp.py
@@ -68,7 +68,15 @@ def save_file(df, output_path, file_type):
     """
     save the DataFrame containing information of papers to a file
     """
-    pass
+    if file_type == 'tsv':
+        df.to_csv(output_path, sep='\t', index=False)
+    elif file_type == 'md':
+        with open(output_path, 'w') as f:
+            f.write(df[['title', 'authors', 'session', 'abstract', 'url']].to_markdown(
+                index=False).replace('   ', ''))  # remove redundant whitespace to shrink the file size
+    else:
+        raise ValueError('Unsupported file type: {}'.format(file_type))
+    print('output saved to {}'.format(output_path))
 
 
 def get_dataframe(paper_list):
