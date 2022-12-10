672ba2132db494033ddccb9db747177dff45fbf4
nmrenyi <renyi1006@gmail.com>
2022-12-10 09:16:22 +0800

    feat: add basic file structure

--- src/parse-dblp.py
+++ src/parse-dblp.py
@@ -3,8 +3,29 @@ script of parsing paper list from dblp
 """
 
 
-def main():
+def parse_args():
     pass
+
+
+def get_page(src):
+    pass
+
+
+def parse_page(page):
+    pass
+
+
+def save_file(df, output_path, file_type):
+    pass
+
+
+def main():
+    args = parse_args()
+    page = get_page(args.src)
+    paper_list = parse_page(page)
+    df = pd.DataFrame(paper_list)[
+        ['title', 'authors', 'session', 'abstract', 'url']]
+    save_file(df, args.output_path, args.type)
 
 
 if __name__ == '__main__':
